ad478719f8339e480a0347ecb784e37b673be460
Justin "J.R." Hill <justin@so.dang.cool>
2023-11-10 03:43:11 -0800

    add a bot

---
+++ bot/serve.rb
@@ -0,0 +1,30 @@
+require 'discordrb'
+require 'open3'
+
+bot = Discordrb::Bot.new(token: ENV.fetch('TOKEN', nil), intents: [:server_messages])
+
+servers = [
+    '1137157403264155719',
+    #'1150472957093744721',
+    #'1141777454164365382'
+]
+
+servers.each do |server|
+    bot.register_application_command(:dt, 'Evaluate dt code', server_id:  server) do |cmd|
+        cmd.string('code', 'the code to evaluate')
+    end
+end
+
+bot.application_command(:dt) do |event|
+    dt_code = event.options['code']
+
+    stdout, status = Open3.capture2(
+        'docker', 'run', '-i', 'booniepepper/dt',
+        'drop', dt_code, 'anything? \pls swap do? quote-all drop quit', stdin_data: ''
+    )
+
+    event.respond(content: "```\n#{event.user.name} ran: #{dt_code}\n---\n#{stdout}\n```")
+end
+
+
+bot.run
