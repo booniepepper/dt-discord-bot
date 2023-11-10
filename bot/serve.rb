f642598905c3204611aede0688b9b22e4ed55c86
Justin "J.R." Hill <justin@so.dang.cool>
2023-11-10 04:11:00 -0800

    add servers

--- bot/serve.rb
+++ bot/serve.rb
@@ -5,13 +5,13 @@ bot = Discordrb::Bot.new(token: ENV.fetch('TOKEN', nil), intents: [:server_messa
 
 servers = [
     '1137157403264155719',
-    #'1150472957093744721',
-    #'1141777454164365382'
+    '1150472957093744721',
+    '1141777454164365382'
 ]
 
 servers.each do |server|
     bot.register_application_command(:dt, 'Evaluate dt code', server_id:  server) do |cmd|
-        cmd.string('code', 'the code to evaluate')
+        cmd.string('code', 'the code to evaluate', required: true)
     end
 end
 
@@ -20,7 +20,7 @@ bot.application_command(:dt) do |event|
 
     stdout, status = Open3.capture2(
         'docker', 'run', '-i', 'booniepepper/dt',
-        'drop', dt_code, 'anything? \pls swap do? quote-all drop quit', stdin_data: ''
+        'drop', dt_code, 'quote-all pls quit', stdin_data: ''
     )
 
     event.respond(content: "```\n#{event.user.name} ran: #{dt_code}\n---\n#{stdout}\n```")
